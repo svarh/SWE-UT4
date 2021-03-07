4ed4221c070e8289c8b236e8302beb00742caba5
Mya Amos <peacockmya@gmail.com>
2021-03-06 22:00:21 -0500

    Now able to check validity when signing up for an account

--- app/src/main/java/com/example/care/SignUp.java
+++ app/src/main/java/com/example/care/SignUp.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -22,6 +23,13 @@ public class SignUp extends AppCompatActivity {
     CheckBox guestCheck;
     CheckBox businessCheck;
     Button signUpButton;
+
+    String email;
+    String password;
+    String name;
+    String companyName;
+    String phoneNumb;
+
 
     static Activity thisActivity = null;
 
@@ -47,6 +55,7 @@ public class SignUp extends AppCompatActivity {
                 companyNameInput.setVisibility(View.INVISIBLE);
             }
         });
+
         businessCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -58,6 +67,38 @@ public class SignUp extends AppCompatActivity {
                 emailInput.setHint("Company Email");
                 phoneInput.setVisibility(View.VISIBLE);
                 companyNameInput.setVisibility(View.VISIBLE);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean validAccount = true;
+                if(!guestCheck.isChecked() & !businessCheck.isChecked()){
+                    makeToast("Please select an account type");
+                }
+                else if(guestCheck.isChecked()){
+                    email = emailInput.getText().toString();
+                    password = passwordInput.getText().toString();
+                    name = nameInput.getText().toString();
+                    phoneNumb = phoneInput.getText().toString();
+                    if(email.length() == 0 | password.length() == 0 | name.length() == 0 | phoneNumb.length() == 0){
+                        makeToast("Please fill in all available fields");
+                    }
+                }
+                else if(businessCheck.isChecked()){
+                    email = emailInput.getText().toString();
+                    password = passwordInput.getText().toString();
+                    name = nameInput.getText().toString();
+                    companyName = companyNameInput.getText().toString();
+                    phoneNumb = phoneInput.getText().toString();
+                    if(email.length() == 0 | password.length() == 0 | name.length() == 0 | companyName.length() == 0 | phoneNumb.length() == 0){
+                        makeToast("Please fill in all available fields");
+                    }
+                    else if(checkValidity(password, email, phoneNumb)){
+                        makeToast("Account Created!");
+                    }
+                }
             }
         });
     }
@@ -73,6 +114,31 @@ public class SignUp extends AppCompatActivity {
         businessCheck = findViewById(R.id.checkBoxBusiness);
         signUpButton = findViewById(R.id.buttonSignUp);
     }
+
+    private boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                .matches();
+    }
+
+    private boolean checkValidity(String password, String email, String phoneNumb){
+        if (password.length() < 6 | password.length() > 12) {
+            makeToast("Password must be between 6 - 12 characters");
+            return false;
+        } else if (!password.equals(confirmPasswordInput.getText().toString())) {
+            makeToast("Passwords do not match");
+            return false;
+        }
+        if (!isEmailValid(email)) {
+            makeToast("Email is invalid");
+            return false;
+        }
+        if (phoneNumb.length() != 10) {
+            makeToast("Phone number is invalid");
+            return false;
+        }
+        return true;
+    }
+
 
     // This method creates Toast to send notification
     public static void makeToast(String str) {
