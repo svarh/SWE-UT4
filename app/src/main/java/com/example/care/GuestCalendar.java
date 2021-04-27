793747b79c56545d4cf8ba6ae4d3782cddf3ee27
Mya Amos <peacockmya@gmail.com>
2021-04-26 21:37:14 -0400

    Guests can now see the scheduled appointment for a certain day in the guest calendar view

--- app/src/main/java/com/example/care/GuestCalendar.java
+++ app/src/main/java/com/example/care/GuestCalendar.java
@@ -1,5 +1,6 @@
 package com.example.care;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -8,6 +9,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GuestCalendar extends AppCompatActivity implements View.OnClickListener{
 
@@ -15,9 +20,15 @@ public class GuestCalendar extends AppCompatActivity implements View.OnClickList
 
     private CalendarView calendarView;
     private Button accountHome;
+    private TextView organization;
+    private TextView officer;
+    private TextView time;
+    private TextView confirmation;
 
     private GuestAccount guest;
     private UserModel userModel;
+
+    private ArrayList<HashMap<String, String>> appointments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +42,39 @@ public class GuestCalendar extends AppCompatActivity implements View.OnClickList
 
         setUI();
 
+        appointments = guest.getAppointments();
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                organization.setText("Organization: \n");
+                officer.setText("Officer: \n");
+                time.setText("Time: ");
+                confirmation.setText("Confirmation: \n");
+
+                String date = (month + 1) + "/" + dayOfMonth + "/" + year;
+                for(int i = 0; i < appointments.size(); i++){
+                    HashMap<String, String> appointment = appointments.get(i);
+                    if(appointment.get("Date").equals(date)){
+                        organization.setText("Organization: \n" + appointment.get("Organization"));
+                        officer.setText("Officer: \n" + appointment.get("Officer"));
+                        time.setText("Time: " + appointment.get("Time"));
+                        confirmation.setText("Confirmation: \n" + appointment.get("Confirmation"));
+                    }
+                }
+            }
+        });
+
     }
 
     public void setUI(){
         calendarView = findViewById(R.id.guestCalendar);
         accountHome = findViewById(R.id.btnAccountHome);
         accountHome.setOnClickListener(this);
+        organization = findViewById(R.id.tvOrg);
+        officer = findViewById(R.id.tvOfficer);
+        time = findViewById(R.id.tvTime);
+        confirmation = findViewById(R.id.tvConfirmation);
     }
 
     @Override
